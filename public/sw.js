0a69206cdda21213eb68568ce6d4c2b7a7cce75d
faveh <juwwarn28@gmail.com>
2018-05-24 06:49:36 +0100

    PWA 80%

--- public/sw.js
+++ public/sw.js
@@ -62,7 +62,7 @@ self.addEventListener('activate', function(event){
 function isInArray(string, array) {
     var cachePath;
     if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
-      // console.log('matched ', string);
+      // console.log('matched ', string); so
       cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
     } else {
       cachePath = string; // store the full request (for CDNs)
